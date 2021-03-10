435ef426bd66019e1f62f92ad9abd350643a90ee
Luis Gabriel Urueta Alvarez <lgurueta@misena.edu.co>
2021-03-10 17:59:08 -0500

    Ajuste v1
    Se reduce peso de anexo PDF y realizan algunas ajustes compartidos por el diseñador.

---
+++ source/js/pages/content.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  alert("mensaje chimbo!");
+}
+
+$('.panel-collapse').on('shown.bs.collapse', function(e) {
+  var $panel = $(this).closest('.panel');
+  $('html,body').animate({
+    scrollTop: $panel.offset().top
+  }, 500);
+});
+
